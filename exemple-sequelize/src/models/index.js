60012f3ea36de0a25cff0bf2afb4344f40ffd5e3
JPG <jordi.pala@gmail.com>
2025-02-24 22:46:55 +0100

    Versió prèvia

---
+++ exemple-sequelize/src/models/index.js
@@ -0,0 +1,28 @@
+/**
+ * index.js de models
+ * Configuració de les relacions entre els models
+ */
+
+const Youtuber = require('./Youtuber');
+const PerfilYoutuber = require('./PerfilYoutuber');
+const Video = require('./Video');
+const Categoria = require('./Categoria');
+
+// Relació 1:1 entre Youtuber i PerfilYoutuber
+Youtuber.hasOne(PerfilYoutuber, { foreignKey: 'youtuber_id' });
+PerfilYoutuber.belongsTo(Youtuber, { foreignKey: 'youtuber_id' });
+
+// Relació 1:N entre Youtuber i Video
+Youtuber.hasMany(Video, { foreignKey: 'youtuber_id' });
+Video.belongsTo(Youtuber, { foreignKey: 'youtuber_id' });
+
+// Relació N:M entre Video i Categoria
+Video.belongsToMany(Categoria, { through: 'videos_categories', foreignKey: 'video_id' });
+Categoria.belongsToMany(Video, { through: 'videos_categories', foreignKey: 'categoria_id' });
+
+module.exports = {
+  Youtuber,
+  PerfilYoutuber,
+  Video,
+  Categoria
+};
